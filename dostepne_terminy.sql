8dc5c2f9c5a1db6873da531d991a310e8f98a667
Lolus147 <79139816+Lolus147@users.noreply.github.com>
2021-03-01 09:30:33 +0100

    Add files via upload

---
+++ dostepne_terminy.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 01 Mar 2021, 09:30
+-- Wersja serwera: 10.4.14-MariaDB
+-- Wersja PHP: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `szczepienia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `dostepne_terminy`
+--
+
+CREATE TABLE `dostepne_terminy` (
+  `id` int(11) NOT NULL,
+  `miesiac` int(11) NOT NULL,
+  `dzien` int(11) NOT NULL,
+  `godzina` time NOT NULL,
+  `zajety` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `dostepne_terminy`
+--
+ALTER TABLE `dostepne_terminy`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `dostepne_terminy`
+--
+ALTER TABLE `dostepne_terminy`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
