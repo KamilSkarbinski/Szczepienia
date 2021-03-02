61e1817e8c6b9cca4361937a7386c8f4e0f8b45e
MorlusInc <79140137+MorlusInc@users.noreply.github.com>
2021-03-02 10:48:44 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -5,14 +5,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Strona Główna</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
 <h1>Witaj Użytkowniku!</h1>
     <nav>
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="terminy.php">Wyznacz termin</li>
+            <li>Strona głowna</li>
+            <li><a href="szczegoly_konta.php"Szczegóły konta></li>
         </ul>
     </nav>
 </body>
