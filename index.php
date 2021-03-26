55f7606600e11564e111c7630742b2853fc4b0b7
Lolus147 <79139816+Lolus147@users.noreply.github.com>
2021-03-26 18:05:02 +0100

    strona glowna

--- index.php
+++ index.php
@@ -7,16 +7,17 @@ if(isset($_POST['next'])){
     $sql = "SELECT `email`, `haslo` FROM `dane_uzytkownika`";
     $result = mysqli_query($base,$sql);
     while($row = mysqli_fetch_array($result))
-        if($row[0] == $email && $row[1] == $hasło){           
-            $imie = $row[2];
+        if($row[0] == $email && $row[1] == $hasło){
+            $login = $_POST['email'];
+            $_SESSION['email']=$row[0];          
         }
 
-    if(!isset($email)){
+    if(!isset($login)){
         header('Location: logowanie.php?status=failed');
     }
 }
 if(isset($_POST['email'])){
-    $email=$_POST['email'];
+    $login = $_POST['email'];
 }
 ?>
 
@@ -34,19 +35,30 @@ if(isset($_POST['email'])){
     <div id="main">
         <div id="menu">
             <ul class="menu">
-                <li><a href="zarezerwuj.php">Wybierz termin</a></li>
-                <li><a href="szczegoly_konta.php">Szczegóły konta</a></li>
+                <li><a href="index.php">Strona główna</a></li>
+                <li><a href="https://www.gov.pl/web/szczepimysie/mapa-punktow-szczepien#/" target="_blank">Punkty szczepień</a></li>
                 <li><a href="wyloguj.php">Wyloguj</a></li>
+                <div id="menu_right">
+                    <li id="login"><a href="szczegoly_konta.php"><img src="avatar.png" alt="Avatar" class="avatar"></a></li>
+                </div>
             </ul>
         </div>
         <div>
-            <div class="container">   
-                <h1 class="main-title">Witaj w Panelu użytkownika <?php echo"$email"?></h1>
-                <img src="szczepionka.jpeg" alt="szczepionkacovid">
-                <footer class="footer">
-                    Copyright © 2021 szczepienia
-                </footer>
-            </div>
+            <h1>Witaj <?php echo $_SESSION['email']?></h1>
+            <div class="bg-img">
+                <div class="container">
+                    <form action="przydzielanie.php" method="POST">
+                        <input class="box" type="submit" value="Wybierz termin" style="font-size:36px">
+                    </form>
+                    <form action="szczegoly_konta" method="POST">
+                        <input type="hidden" value=<?php $_SESSION['email']?> name="login">
+                    </form>
+                </div>
+            </div>    
+            <footer class="footer">
+                </br></br>
+                Copyright © 2021 szczepienia
+            </footer>
         </div>
     </div>
 </body>
