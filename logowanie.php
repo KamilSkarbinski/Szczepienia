fa11edcb73afa564e4e40ad0c55c0cab8d64b20b
Lolus147 <79139816+Lolus147@users.noreply.github.com>
2021-03-26 18:05:21 +0100

    logowanie - wersja koncowa

--- logowanie.php
+++ logowanie.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="styleform.css">
+    <link rel="stylesheet" href="style1.css">
     <title>Logowanie użytkownika</title>
 </head>
 <body>
@@ -16,16 +16,16 @@
                     <b>Hasło:</b><br/><br/> <input class="pole" type="password" name="haslo" required><br/><br/>
                     <br/>
                     <div id="dol">
-                        <input id="logowanie" type="submit" name="next" value="Zaloguj">
+                        <button id="logowanie" type="submit" name="next">Zaloguj</button>
                         <br/>
                         <?php 
                         if(isset($_GET['status'])){
                             if($_GET['status'] == "failed")
-                                echo "Niepoprawna nazwa lub hasło użytkownika";
+                                echo "<div id='komunikat'>Niepoprawna nazwa lub hasło użytkownika</div>";
                         }
                         ?>
-                        <div id="link2">
-                            <a href="dodawaniedobazy.php">Nie masz konta? Zarejestruj się</a>
+                        <div id="link">
+                            <a href="rejestracja.php">Nie masz konta?</br>Zarejestruj się</a>
                         </div>
                     </div>
                 </form>
