f6ffe484ccd15aa495181fcd82d05e9744681ab3
KamilSkarbinski <79136611+KamilSkarbinski@users.noreply.github.com>
2021-04-10 08:46:34 +0200

    Update rezerwacja.php

--- rezerwacja.php
+++ rezerwacja.php
@@ -1,11 +1,11 @@
 <?php
 class Reservation {
-  // (A) PROPERTIES
+  
   private $pdo; // PDO object
   private $stmt; // SQL statement
   public $error; // Error message
 
-  // (B) CONSTRUCTOR - CONNECT TO DATABASE
+  
   function __construct() {
     try {
       $this->pdo = new PDO(
@@ -15,21 +15,14 @@ class Reservation {
     } catch (Exception $ex) { die($ex->getMessage()); }
   }
 
-  // (C) DESTRUCTOR - CLOSE DATABASE CONNECTION
   function __destruct() {
     $this->pdo = null;
     $this->stmt = null;
   }
 
-  // (D) SAVE RESERVATION
-  function save ($date, $slot, $name, $email, $tel, $notes="") {
-    // (D1) CHECKS & RESTRICTIONS
-    // @TODO - ADD YOUR OWN RULES & REGULATIONS HERE
-    // MAX # OF RESERVATIONS ALLOWED?
-    // USER CAN ONLY BOOK X DAYS IN ADVANCE?
-    // USER CAN ONLY BOOK A MAX OF X SLOTS WITHIN Y DAYS?
 
-    // (D2) DATABASE ENTRY
+  function save ($date, $slot, $name, $email, $tel, $notes="") {
+
     try {
       $this->stmt = $this->pdo->prepare(
         "INSERT INTO `reservations` (`res_date`, `res_slot`, `res_name`, `res_email`, `res_tel`, `res_notes`) VALUES (?,?,?,?,?,?)"
@@ -40,36 +33,43 @@ class Reservation {
       return false;
     }
 
-    // (D3) EMAIL
-    // @TODO - REMOVE IF YOU WANT TO MANUALLY CALL TO CONFIRM OR SOMETHING
-    // OR EMAIL THIS TO A MANAGER OR SOMETHING
+
     $subject = "Reservation Received";
     $message = "Thank you, we have received your request and will process it shortly.";
     @mail($email, $subject, $message);
     return true;
   }
   
-  // (E) GET RESERVATIONS FOR THE DAY
+
   function getDay ($day="") {
-    // (E1) DEFAULT TO TODAY
+    
     if ($day=="") { $day = date("Y-m-d"); }
     
-    // (E2) GET ENTRIES
+   
     $this->stmt = $this->pdo->prepare(
       "SELECT * FROM `reservations` WHERE `res_date`=?"
     );
     $this->stmt->execute([$day]);
     return $this->stmt->fetchAll(PDO::FETCH_NAMED);
   }
+    function res_count($date,$slot){
+    $ppl=0;
+    $query = $this->pdo->prepare(
+      "SELECT * FROM `reservations` WHERE `res_date` like ? and `res_slot` like ?"
+    );
+    $query->execute([$date,$slot]);
+    foreach($query as $row){
+      $ppl++;
+  }
+  return $ppl;
+  }
+
 }
 
-// (F) DATABASE SETTINGS
-// ! CHANGE THESE TO YOUR OWN !
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'test');
 define('DB_CHARSET', 'utf8');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
-// (G) NEW RESERVATION OBJECT
 $_RSV = new Reservation();
